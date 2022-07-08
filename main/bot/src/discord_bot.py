e2b0445bb7128d86ac8f9a95d49509217f22dd99
konpeki <92644639+konpeki02@users.noreply.github.com>
2022-07-07 21:53:10 -0500

    added to quote command to send embedded image with quote, user, and time

--- main/bot/src/discord_bot.py
+++ main/bot/src/discord_bot.py
@@ -1,8 +1,10 @@
 import json
-import discord
 import sys
+import discord
 from discord.ext import commands
+from datetime import datetime
 
+bot = commands.Bot(command_prefix='!')
 
 
 def getKey(filename, target):
@@ -19,11 +21,6 @@ def nextWord(target, source):
             return msg[i + 1]
 
 
-key = getKey('key.json', 'key')
-
-bot = commands.Bot(command_prefix='!')
-
-
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
@@ -31,6 +28,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    await bot.process_commands(message)
     if message.author == bot.user:
         return
     words = ['Im', 'I\'m', 'im']
@@ -39,8 +37,17 @@ async def on_message(message):
             reply = nextWord(word, message.content)
             await message.channel.send('Hello %s, I\'m Dad!' % reply)
     if message.content.startswith('!exit'):
-        exit()
+        await message.channel.send('Sayonara...\n')
+        sys.exit()
 
 
-bot.run(key)
+@bot.command(pass_context=True)
+async def quote(ctx, arg, name):
+    current_time = datetime.now()
+    timestamp = 'Timestamp: %s' % current_time.strftime("%b-%d-%Y %H:%M:%S")
+    user_quote = '\" %s \" - %s' % (arg, name)
+    embed = discord.Embed(title=user_quote, description=timestamp, color=0x00ff00)
+    await ctx.channel.send(embed=embed)
 
+
+bot.run(getKey('key.json', 'key'))
